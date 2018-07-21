d76c2335a27b898e16ae190035d70ea4ed6f5174
hat <jburns@chapman.edu>
2018-07-21 16:01:42 -0700

    Fixed background scroll

--- themes/GCI/scr/js/no_scroll.js
+++ themes/GCI/scr/js/no_scroll.js
@@ -1,2 +1,11 @@
-//Wa Wa
-console.log("Waa");
+// A work-around to fix bootstrap 4 not preventing background scroll
+// on element #navbarSupportedContent in navbar.html partial
+//V0.0.1
+
+$('#navbarSupportedContent').on('hidden.bs.collapse', function () {
+    $("body").css("overflow","scroll");
+});
+
+$('#navbarSupportedContent').on('show.bs.collapse', function () {
+    $("body").css("overflow","hidden");
+});
