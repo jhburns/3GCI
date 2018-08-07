dcf4669a6b363639798edd85fa6fba66ead3e9bc
hat <jburns@chapman.edu>
2018-08-07 02:03:51 -0700

    Made generator faster

--- build_scripts/thumb_gen.js
+++ build_scripts/thumb_gen.js
@@ -1,26 +1,36 @@
 // Thumbnail scraper
-var version = '0.0.3';
+
+// All files are read in, and all images are processed asynchronously so this script is super fast
+// Main bottleneck is internet speed
+
+var version = '0.0.5';
+
+// input/output
 const colors = require('colors');
 const input_folder = './data/links/';
 const output_folder = './themes/GCI/scr/img/thumbnails/';
 
 console.log(colors.green("\nThumbnail generator ") + version);
 
+// Needed to figure out which img is best from a url
 var Scraper = require('scraper-js');
 var scraper = new Scraper.Scraper();
 
-
+// Nedded to read in file and convert toml to javascript object
 var fs = require('fs');
 var toml = require('toml');
 
+// Pretty clearly needed to download img
 var download = require('download-file');
 
+//get all dirs
 fs.readdir(input_folder, (err, files) => {
     if (err) {
         console.log(colors.red("Error getting files:"));
         console.log(err);
         return;
     } else {
+        //for each file
         files.forEach(function(filename) {
             fs.readFile(input_folder + filename, 'utf-8', function(err, content) {
                 if (err) {
@@ -30,8 +40,9 @@ fs.readdir(input_folder, (err, files) => {
                 }
                 
                 try {
+                    //toml parsing
                     var file = toml.parse(content);
-                    if (file.hasOwnProperty('link')) {
+                    if (file.hasOwnProperty('link') && file.hasOwnProperty('enable_thumb') && file.hasOwnProperty('thumb_img')) {
                         scraper.scrape(file.link).then(function (thumbs) {
 
                             var options = {
@@ -39,14 +50,19 @@ fs.readdir(input_folder, (err, files) => {
                                 filename: file.thumb_img + ".thm"
                             };
 
-                            download(thumbs[0], options, function(err){
-                                if (err) {
-                                    console.log(colors.red("Error: could not download"));
-                                    console.log(err);
-                                }
-                                console.log(colors.bold("Created: ") + options.filename + colors.bold(" For: ") + filename);
+                            if (file.enable_thumb) {
+                                //image downloading
+                                download(thumbs[0], options, function (err) {
+                                    if (err) {
+                                        console.log(colors.red("Error: could not download"));
+                                        console.log(err);
+                                    }
 
-                            });
+                                    //Output
+                                    console.log(colors.bold("Created: ") + options.filename + colors.bold(" For: ") + filename);
+
+                                });
+                            }
 
                         });
                     }
